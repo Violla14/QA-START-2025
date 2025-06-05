3a0a782dd3fa19b5ae1f34389b821e564cb64375
Violla14 <ninokkns@gmail.com>
2025-06-05 11:10:45 -0600

    HomeWork6_1

---
+++ src/main/java/org/prog/Plane.java
@@ -0,0 +1,10 @@
+package org.prog;
+
+public class Plane {
+    public String flightId;
+    public String currentPassengersSet;
+    public String maxPassengersSet; }
+
+
+
+
--- src/main/java/org/prog/Session6HomeWork.java
+++ src/main/java/org/prog/Session6HomeWork.java
@@ -1,4 +1,15 @@
 package org.prog;
 
 public class Session6HomeWork {
-}
+        public static void main(String[] args) {
+            Plane plane1 = new Plane();
+
+            plane1.flightId = "AA-101";
+            plane1.currentPassengersSet = "100";
+            plane1.maxPassengersSet = "15";
+
+            System.out.println("flight ID: " + plane1.flightId + " | currentPassengersSet: " + plane1.currentPassengersSet + " | maxPassengersSet: " + plane1.maxPassengersSet);
+        }
+    }
+
+
