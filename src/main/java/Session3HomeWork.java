9eb6bd811cfe69a0bf05b1534f5151e0ef0d393e
Violla14 <ninokkns@gmail.com>
2025-06-09 23:05:35 -0600

    HW3

--- src/main/java/Session3HomeWork.java
+++ src/main/java/Session3HomeWork.java
@@ -1,2 +1,45 @@
+import java.util.Random;
+
 public class Session3HomeWork {
+    public static void main(String[] args) {
+        int[] hotelRooms = new int[15];
+        Random rand = new Random();
+
+        // Заповнення масиву випадковими числами
+        for (int i = 0; i < hotelRooms.length; i++) {
+            hotelRooms[i] = rand.nextInt(25); // значення від 0 до 24
+        }
+
+        // Вивід ДО сортування
+        System.out.print("До сортування:    ");
+        for (int i = 0; i < hotelRooms.length; i++) {
+            System.out.print(hotelRooms[i] + " ");
+        }
+        System.out.println();
+
+        // Сортування за спаданням
+        boolean unsorted = true;
+
+        while (unsorted) {
+            boolean elementsSwitched = false;
+
+            for (int i = 0; i < hotelRooms.length - 1; i++) {
+                if (hotelRooms[i + 1] > hotelRooms[i]) {
+                    int temp = hotelRooms[i];
+                    hotelRooms[i] = hotelRooms[i + 1];
+                    hotelRooms[i + 1] = temp;
+                    elementsSwitched = true;
+                }
+            }
+
+            unsorted = elementsSwitched;
+        }
+
+        // Вивід ПІСЛЯ сортування
+        System.out.print("Після сортування: ");
+        for (int i = 0; i < hotelRooms.length; i++) {
+            System.out.print(hotelRooms[i] + " ");
+        }
+        System.out.println();
+    }
 }
